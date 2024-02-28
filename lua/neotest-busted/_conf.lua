7d44593e8a0b0410a3337300c12adb9f67564ef3
HiPhish <hiphish@posteo.de>
2024-02-28 20:53:20 +0100

    Move reading configuration to dedicated function
    In case we might have to read configuration files in other places as
    well.  Reading has to be secure, so it's better to have one function
    that has been secured rather than secure several ad-hoc reads.

--- lua/neotest-busted/_conf.lua
+++ lua/neotest-busted/_conf.lua
@@ -6,20 +6,79 @@
 ---for too long because a new table might become the adapter settings.
 local M = {}
 
+---User configuration for running busted.
+---@class neotestBusted.Config
+---@field ROOT      string[]?
+---@field pattern   string?
+---@field directory string?
+---@field output    string?
+---@field tags      string[]?
+---@field filter    string[]?
+---@field lpath     string?
+---@field cpath     string?
+---@field run       string?
+---@field repeat    integer?
+---@field seed      integer?
+---@field lang      string?
+---@field helper    string?
+---@field lua       string?
+---@field Xoutput   string[]?
+---@field Xhelper   string[]?
+---@field coverage  boolean?
+---@field verbose   boolean?
+---@field lazy      boolean?
+---@field recursive boolean?
+---@field sort      boolean?
+---@field shuffle   boolean?
+---@field ['config-file']      string?
+---@field ['exclude-tags']     string[]?
+---@field ['filter-out']       string[]?
+---@field ['enable-sound']     string[]?
+---@field ['ignore-lua']       string[]?
+---@field ['auto-insulate']    string[]?
+---@field ['keep-going']       string[]?
+---@field ['shuffle-files']    string[]?
+---@field ['shuffle-tests']    string[]?
+---@field ['sort-files']       string[]?
+---@field ['sort-tests']       string[]?
+---@field ['suppress-pending'] string[]?
+---@field ['defer-print']      string[]?
+
+
+---The current busted configuration.
+---@type neotestBusted.Config
 local conf = {}
 
 ---Default configuration.  The values are taken from the help text of busted.
+---@type neotestBusted.Config
 M.default = {
 	pattern = '_spec',
 }
 
----Returns the internal settings table.
----@return table
+---Attempts to read the user's configuration from the `.busted` file.
+---@param root string  Path to the root of the project
+---@return neotestBusted.Config? config  Configuration read from file or `nil`
+function M.read(root)
+	local path = string.format('%s/.busted', root)
+	if not vim.fn.filereadable(path) then return end
+	if not vim.secure.read(path) then return end
+
+	local result = loadfile(path)()
+	local t = type(result)
+	if t ~= 'table' then
+		error(string.format('Busted configuration is of type %s, but it needs to be table', t))
+	end
+	return result
+end
+
+---Returns the currently active busted configuration.
+---@return neotestBusted.Config
 function M.get()
 	return conf
 end
 
----@param other table  New settings table
+---Set the current active busted configuration.
+---@param other neotestBusted.Config  New busted configuration
 function M.set(other)
 	conf = other
 end
